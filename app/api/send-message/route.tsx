aa9a4d87e26375dc045edbc0528f57e4bcebf00c
saumya11mehta <115432888+saumya11mehta@users.noreply.github.com>
2024-08-30 10:16:22 -0400

    Changed api model to flash

--- app/api/send-message/route.tsx
+++ app/api/send-message/route.tsx
@@ -73,7 +73,7 @@ async function getModelResponse(history: IHistory[], currentMessage : string){
 		// Access your API key as an environment variable (see "Set up your API key" above)
 		const genAI = new GoogleGenerativeAI(process.env.API_KEY as string);
 
-		const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+		const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
 		const chat = model.startChat({
 			history: history,
